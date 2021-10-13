2a73199f8472ca13afbe4d1e61a419906d38f2d4
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-10-14 00:48:52 +0300

    last efforts for the night

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import csv
-import math
 
 
 def read_csv_file(file_name):
     """
     Reads a csv file after second line and returns three list of lists based on the second column
     """
-    ones = []
-    twos = []
-    threes = []
+    numbers = []
     with open(file_name, 'r') as csv_file:
 
         csv_reader = csv.reader(csv_file)
         next(csv_reader)
         
         for item in list(csv_reader):
-            if item[1] == 1:
-                ones.append(int(item[1]))
-            elif item[1] == 2:
-                twos.append(int(item[1]))
-            elif item[1] == 3:
-                threes.append(int(item[1]))
-
-    return ones, twos, threes
+            my_dict = { 'value': item[0], 'class': item[1] , 'prediction' : -1 }
+            numbers.append(my_dict)
+    return numbers
 
 
 def sum_numbers(numbers):
@@ -39,6 +31,7 @@ def my_mean(numbers):
     """
     Calculates the mean of a list of numbers
     """
+
     return sum_numbers(numbers) / len(numbers)
 
 
@@ -51,40 +44,24 @@ def standard_deviation(numbers):
     return variance ** 0.5
 
 
-def calculate_likelihoods(numbers):
+def calculate_appearances(numbers, class_id):
     """
-    Calculates the likelihoods of a list of list of numbers
+    Calculates how many times an item that belongs to a class appears in a list
     """
-    likelihoods = []
-    for a_set in numbers:
-        mn = my_mean(a_set)
-        std = standard_deviation(a_set)
-
-        # Calculate the likelihood of each number in the set
-        likelihoods.append([1 / (std * (2 * 3.14) ** 0.5) * (math.exp(-((x - mn) ** 2) / (2 * std ** 2))) for x in a_set])
-    return likelihoods
+    appear = 0
+    for dict in numbers:
+        if dict['class'] == class_id:
+            appear += 1
+    return appear
 
 
-ones, twos, threes = read_csv_file("testing.csv")
-count = len(ones) + len(twos) + len(threes)
-data = [ones, twos, threes]
+def calculate_prior(numbers, class_id):
+    """
+    Calculates the prior of a class
+    """
+    appearances = calculate_appearances(numbers, class_id)
+    total = len(numbers)
+    return appearances / total
 
-prior_one = len(ones) / count
-prior_two = len(twos) / count
-prior_three = len(threes) / count
 
-mean_one = my_mean(ones)
-mean_two = my_mean(twos)
-mean_three = my_mean(threes)
-
-likelihoods = calculate_likelihoods(data)
-
-for i in range(len(data[0])):
-    highest_posterior = -1.0
-
-    for j in range(len(data)):
-        posterior = prior_one * likelihoods[j][i]
-        if posterior > highest_posterior:
-            highest_posterior = posterior
-        
-
+numbers = read_csv_file("testing.csv")
