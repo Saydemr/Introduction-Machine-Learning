d88ab9795a1ec62bfa17adcb659a984dd2ec89ff
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-10-13 16:12:06 +0300

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import csv
+import math
+
+
+
 
 def read_csv_file(file_name):
     """
@@ -23,19 +27,57 @@ def read_csv_file(file_name):
     return ones, twos, threes
 
 
-def standard_deviation(numbers):
+def sum_numbers(numbers):
     """
-    Calculates the standard deviation of a list of numbers
+    Sums all the numbers in a list
     """
-    average = sum(numbers) / len(numbers)
-    variance = sum([(x - average) ** 2 for x in numbers]) / len(numbers)
-    return variance ** 0.5
+    sum = 0
+    for number in numbers:
+        sum += number
+    return sum
+    
 
 def mean(numbers):
     """
     Calculates the mean of a list of numbers
     """
-    return sum(numbers) / len(numbers)
+    return sum_numbers(numbers) / len(numbers)
+
+
+def standard_deviation(numbers):
+    """
+    Calculates the standard deviation of a list of numbers
+    """
+    average = mean(numbers)
+    variance = sum_numbers([(x - average) ** 2 for x in numbers]) / len(numbers)
+    return variance ** 0.5
+
+
+def likelihoods(numbers):
+    """
+    Calculates the likelihoods of a list of list of numbers
+    """
+    likelihoods = []
+    for a_set in numbers:
+        mn = mean(a_set)
+        std = standard_deviation(a_set)
+        likelihoods.append([1 / (std * (2 * 3.14) ** 0.5) * (math.exp(-((x - mn) ** 2) / (2 * std ** 2))) for x in a_set])
+    return likelihoods
+
+def posterior(numbers):
+    """
+    Calculates the posterior of a list of numbers
+    """
+    all_data = likelihoods(numbers)
+    posterior = []
+
+    count = len(numbers[0]) + len(numbers[1]) + len(numbers[2])
+    prior_one = len(numbers[0]) / count
+    prior_two = len(numbers[1]) / count
+    prior_three = len(numbers[2]) / count
+    
+
+    for i in range(len(all_data)):
 
 
 
@@ -43,9 +85,14 @@ ones, twos, threes = read_csv_file("testing.csv")
 count = len(ones) + len(twos) + len(threes)
 data = [ones, twos, threes]
 
+prior_one = len(ones) / count
+prior_two = len(twos) / count
+prior_three = len(threes) / count
+
+mean_one = mean(ones)
+mean_two = mean(twos)
+mean_three = mean(threes)
+
 for items in data:
-    mn = mean(items)
-    std = standard_deviation(items)
-    print("The mean of the data is: {}".format(mn))
-    print("The standard deviation of the data is: {}".format(std))
+
 
