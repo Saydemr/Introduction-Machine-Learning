b37059a17e6d6a7b9a9130e5a6eb2dffeb83fb01
Saydemr <abdullah.saydemir@ozu.edu.tr>
2021-10-13 23:58:44 +0300

    cpp file

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import csv
 import math
 
 
-
-
 def read_csv_file(file_name):
     """
     Reads a csv file after second line and returns three list of lists based on the second column
@@ -37,7 +35,7 @@ def sum_numbers(numbers):
     return sum
     
 
-def mean(numbers):
+def my_mean(numbers):
     """
     Calculates the mean of a list of numbers
     """
@@ -48,37 +46,23 @@ def standard_deviation(numbers):
     """
     Calculates the standard deviation of a list of numbers
     """
-    average = mean(numbers)
+    average = my_mean(numbers)
     variance = sum_numbers([(x - average) ** 2 for x in numbers]) / len(numbers)
     return variance ** 0.5
 
 
-def likelihoods(numbers):
+def calculate_likelihoods(numbers):
     """
     Calculates the likelihoods of a list of list of numbers
     """
     likelihoods = []
     for a_set in numbers:
-        mn = mean(a_set)
+        mn = my_mean(a_set)
         std = standard_deviation(a_set)
+
+        # Calculate the likelihood of each number in the set
         likelihoods.append([1 / (std * (2 * 3.14) ** 0.5) * (math.exp(-((x - mn) ** 2) / (2 * std ** 2))) for x in a_set])
     return likelihoods
-
-def posterior(numbers):
-    """
-    Calculates the posterior of a list of numbers
-    """
-    all_data = likelihoods(numbers)
-    posterior = []
-
-    count = len(numbers[0]) + len(numbers[1]) + len(numbers[2])
-    prior_one = len(numbers[0]) / count
-    prior_two = len(numbers[1]) / count
-    prior_three = len(numbers[2]) / count
-    
-
-    for i in range(len(all_data)):
-
 
 
 ones, twos, threes = read_csv_file("testing.csv")
@@ -89,10 +73,18 @@ prior_one = len(ones) / count
 prior_two = len(twos) / count
 prior_three = len(threes) / count
 
-mean_one = mean(ones)
-mean_two = mean(twos)
-mean_three = mean(threes)
+mean_one = my_mean(ones)
+mean_two = my_mean(twos)
+mean_three = my_mean(threes)
 
-for items in data:
+likelihoods = calculate_likelihoods(data)
 
+for i in range(len(data[0])):
+    highest_posterior = -1.0
+
+    for j in range(len(data)):
+        posterior = prior_one * likelihoods[j][i]
+        if posterior > highest_posterior:
+            highest_posterior = posterior
+        
 
